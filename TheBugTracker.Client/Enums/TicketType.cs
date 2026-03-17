96925ffbbdb5bfae129ad98d648836043be66cce
justarunsthing <justarunsthing@outlook.com>
2026-03-17 08:27:56 +0000

    Added TicketType enum

---
+++ TheBugTracker.Client/Enums/TicketType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Client.Enums
+{
+    public enum TicketType
+    {
+        [Display(Name = "New Development")]
+        NewDevelopment,
+        [Display(Name = "Work Task")]
+        WorkTask,
+        Bug,
+        [Display(Name = "Change Request")]
+        ChangeRequest,
+        Enhancement,
+        [Display(Name = "General Task")]
+        GeneralTask
+    }
+}
