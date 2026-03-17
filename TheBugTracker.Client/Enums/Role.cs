0a8d8fc4fa7a10d6253d7517cd26ccf5866f8850
justarunsthing <justarunsthing@outlook.com>
2026-03-17 08:24:11 +0000

    Added Role enum

---
+++ TheBugTracker.Client/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Client.Enums
+{
+    public enum Role
+    {
+        Admin,
+        [Display(Name = "Project Manager")]
+        ProjectManager,
+        Developer,
+        Submitter,
+        [Display(Name = "Demo User")]
+        DemoUser
+    }
+}
