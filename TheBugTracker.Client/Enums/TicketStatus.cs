18909b2fa030e4dbdb5b9ae926ff6dc9e29adf01
justarunsthing <justarunsthing@outlook.com>
2026-03-17 08:29:01 +0000

    Added TicketStatus enum

---
+++ TheBugTracker.Client/Enums/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Client.Enums
+{
+    public enum TicketStatus
+    {
+        New,
+        [Display(Name = "In Development")]
+        InDevelopment,
+        Testing,
+        Resolved
+    }
+}
