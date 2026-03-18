87bfaaa6da822dcf92473768d51456c55fcff590
justarunsthing <justarunsthing@outlook.com>
2026-03-18 08:02:00 +0000

    Added Project class

---
+++ TheBugTracker/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using TheBugTracker.Client.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public ProjectPriority Priority { get; set; }
+        public bool IsArchived { get; set; } = false;
+
+        // Navigational Properties
+        public int CompanyId { get; set; }
+        public virtual Company? Company { get; set; }
+        public virtual ICollection<ApplicationUser> Members { get; set; } = [];
+    }
+}
