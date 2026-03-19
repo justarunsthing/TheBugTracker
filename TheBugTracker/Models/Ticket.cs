c01a5654dc3ab438c8f9a6a7c8e756ab37f58a39
justarunsthing <justarunsthing@outlook.com>
2026-03-19 07:56:29 +0000

    Added Ticket model

---
+++ TheBugTracker/Models/Ticket.cs
@@ -0,0 +1,46 @@
+﻿using TheBugTracker.Client.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Ticket
+    {
+        private DateTimeOffset _created;
+        private DateTimeOffset? _updated;
+
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+        public DateTimeOffset Created 
+        { 
+            get => _created; 
+            set => _created = value.ToUniversalTime(); 
+        }
+
+        public DateTimeOffset? Updated 
+        { 
+            get => _updated;
+            set => _updated = value?.ToUniversalTime(); 
+        }
+
+        public bool IsArchived { get; set; } = false;
+        public bool IsArchivedByProject { get; set; } = false;
+        public TicketPriority Priority { get; set; }
+        public TicketType Type { get; set; }
+        public TicketStatus Status { get; set; }
+
+        // Navigational Properties
+        public int ProjectId { get; set; }
+        public virtual Project? Project { get; set; }
+
+        [Required]
+        public string? SubmitterUserId { get; set; }
+        public virtual ApplicationUser? SubmitterUser { get; set; }
+        public string? DeveloperUserId { get; set; }
+        public virtual ApplicationUser? DeveloperUser { get; set; }
+    }
+}
