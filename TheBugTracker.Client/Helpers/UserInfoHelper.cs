9486b9ec9e37102a3265d388b142c65420a49d01
justarunsthing <justarunsthing@outlook.com>
2026-04-06 10:56:17 +0100

    Mapped properties to claims

--- TheBugTracker.Client/Helpers/UserInfoHelper.cs
+++ TheBugTracker.Client/Helpers/UserInfoHelper.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using System.Security.Claims;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace TheBugTracker.Client.Helpers
 {
@@ -19,15 +19,21 @@ namespace TheBugTracker.Client.Helpers
             {
                 var userId = user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
                 var email = user.FindFirst(ClaimTypes.Email)!.Value;
-                var firstName = user.FindFirst("FirstName")!.Value;
-                var lastName = user.FindFirst("LastName")!.Value;
+                var firstName = user.FindFirst(nameof(UserInfo.FirstName))!.Value;
+                var lastName = user.FindFirst(nameof(UserInfo.LastName))!.Value;
+                var companyId = user.FindFirst(nameof(UserInfo.CompanyId))!.Value;
+                var profilePictureUrl = user.FindFirst(nameof(UserInfo.ProfilePictureUrl))!.Value;
+                var roles = user.FindAll(nameof(UserInfo.Roles)).Select(r => r.Value);
 
                 return new UserInfo
                 {
                     UserId = userId,
                     Email = email,
                     FirstName = firstName,
-                    LastName = lastName
+                    LastName = lastName,
+                    CompanyId = int.Parse(companyId),
+                    ProfilePictureUrl = profilePictureUrl,
+                    Roles = [.. roles]
                 };
             }
             catch
--- TheBugTracker/Components/Account/CustomUserClaimsPrincipalFactory.cs
+++ TheBugTracker/Components/Account/CustomUserClaimsPrincipalFactory.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using TheBugTracker.Client;
 using TheBugTracker.Models;
 
 namespace TheBugTracker.Components.Account
 {
-    public class CustomUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager, IOptions<IdentityOptions> options)
-        : UserClaimsPrincipalFactory<ApplicationUser>(userManager, options)
+    public class CustomUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager,
+                                                  RoleManager<IdentityRole> roleManager,
+                                                  IOptions<IdentityOptions> options)
+        : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>(userManager, roleManager, options)
     {
+        // Method gets called automatically when a user logs in and generates the claims for that user
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
         {
             ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
-            List<Claim> customClaims =
+
+            string profilePictureUrl = user.ProfilePictureId.HasValue
+                ? $"uploads/{user.ProfilePictureId}"
+                : $"https://api.dicebear.com/9.x/glass/svg?seed={user.Id}";
+
+            List <Claim> customClaims =
             [
-                new Claim("FirstName", user.FirstName ?? string.Empty),
-                new Claim("LastName", user.LastName ?? string.Empty)
+                new Claim(nameof(UserInfo.FirstName), user.FirstName!),
+                new Claim(nameof(UserInfo.LastName), user.LastName!),
+                new Claim(nameof(UserInfo.CompanyId), user.CompanyId.ToString()!),
+                new Claim(nameof(UserInfo.ProfilePictureUrl), profilePictureUrl)
             ];
 
             identity.AddClaims(customClaims);
