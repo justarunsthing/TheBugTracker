052c4c1b2bb7f4bf5b52b701e679160b8d77b1cd
justarunsthing <justarunsthing@outlook.com>
2026-04-12 09:35:18 +0100

    Implemented CreateProjectAsync method

--- TheBugTracker/Interfaces/IProjectRepository.cs
+++ TheBugTracker/Interfaces/IProjectRepository.cs
@@ -10,5 +10,16 @@ namespace TheBugTracker.Interfaces
         /// </summary>
         /// <param name="user>The current user's claims </param>
         Task<IEnumerable<Project>> GetProjectsAsync(UserInfo user);
+
+        /// <summary>
+        /// Creates a new project in the database for the user's company
+        /// </summary>
+        /// <remarks>
+        /// Only project managers and admins roles can create new projects
+        /// </remarks>
+        /// <param name="project">The project to be saved in the database</param>
+        /// <param name="user">The current user's claims</param>
+        /// <returns>The created project after it has been saved in the database</returns>
+        Task<Project> CreateProjectAsync(Project project, UserInfo user);
     }
 }
--- TheBugTracker/Repository/ProjectRepository.cs
+++ TheBugTracker/Repository/ProjectRepository.cs
@@ -2,6 +2,7 @@
 using TheBugTracker.Models;
 using TheBugTracker.Client;
 using TheBugTracker.Interfaces;
+using TheBugTracker.Client.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace TheBugTracker.Repository
@@ -17,6 +18,34 @@ namespace TheBugTracker.Repository
                 .ToListAsync();
 
             return projects;
+        }
+
+        public async Task<Project> CreateProjectAsync(Project project, UserInfo user)
+        {
+            bool isAdmin = user.Roles.Any(r => r == nameof(Role.Admin));
+            bool isPm = user.Roles.Any(r => r == nameof(Role.ProjectManager));
+
+            if (!isAdmin && !isPm)
+            {
+                throw new ApplicationException($"User {user.Email} is not authorized to create a project because they are not an Admin or a Project Manager");
+            }
+
+            await using ApplicationDbContext context = contextFactory.CreateDbContext();
+
+            project.CompanyId = user.CompanyId;
+            project.Created = DateTimeOffset.UtcNow;
+
+            if (isPm)
+            {
+                ApplicationUser projectManager = await context.Users.FirstAsync(u => u.Id == user.UserId);
+
+                project.Members.Add(projectManager);
+            }
+
+            context.Add(project);
+            await context.SaveChangesAsync();
+
+            return project;
         }
     }
 }
