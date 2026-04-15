e7568d3ea562704d9bbd21142b8a90103b97f198
justarunsthing <justarunsthing@outlook.com>
2026-04-15 08:20:09 +0100

    Added GetColor extension method

---
+++ TheBugTracker.Client/Helpers/MudBlazorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazor;
+using TheBugTracker.Client.Enums;
+
+namespace TheBugTracker.Client.Helpers
+{
+    public static class MudBlazorExtensions
+    {
+        public static Color GetColor(this ProjectPriority priority)
+        {
+            Color color = priority switch
+            {
+                ProjectPriority.Low => Color.Success,
+                ProjectPriority.Medium => Color.Secondary,
+                ProjectPriority.High => Color.Error,
+                ProjectPriority.Urgent => Color.Dark,
+                _ => Color.Default
+            };
+
+            return color;
+        }
+    }
+}
