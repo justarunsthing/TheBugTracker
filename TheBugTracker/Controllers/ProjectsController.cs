f78dc17b38c0e8c94a11f091ad463973ff3160b2
justarunsthing <justarunsthing@outlook.com>
2026-04-25 09:04:23 +0100

    Added ProjectsController & GetProjects endpoint

---
+++ TheBugTracker/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using TheBugTracker.Client;
+using Microsoft.AspNetCore.Mvc;
+using TheBugTracker.Client.Models;
+using TheBugTracker.Client.Helpers;
+using TheBugTracker.Client.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TheBugTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProjectsController(IProjectDTOService projectService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProjectDTO>>> GetProjects()
+        {
+            UserInfo userInfo = UserInfoHelper.GetUserInfo(User)!;
+            var projects = await projectService.GetProjectsAsync(userInfo);
+
+            return Ok(projects);
+        }
+    }
+}
