12662e96c16d33cb345a91b8abd4173627d907d7
justarunsthing <justarunsthing@outlook.com>
2026-04-26 10:24:06 +0100

    Added skeleton WASMProjectDTOService class

---
+++ TheBugTracker.Client/Services/WASMProjectDTOService.cs
@@ -0,0 +1,38 @@
+﻿using TheBugTracker.Client.Models;
+using TheBugTracker.Client.Interfaces;
+
+namespace TheBugTracker.Client.Services
+{
+    public class WASMProjectDTOService(HttpClient http) : IProjectDTOService
+    {
+        public Task<ProjectDTO?> GetProjectByIdAsync(int projectId, UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProjectDTO>> GetProjectsAsync(UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectDTO> CreateProjectAsync(ProjectDTO project, UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProjectAsync(ProjectDTO project, UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ArchiveProjectAsync(int projectId, UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RestoreProjectAsync(int projectId, UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
