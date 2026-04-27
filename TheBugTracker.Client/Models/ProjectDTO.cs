a078c8b73139f6999cd3ebd68eb07f251f85e659
justarunsthing <justarunsthing@outlook.com>
2026-04-27 08:17:53 +0100

    Enhance documentation

--- TheBugTracker.Client/Models/ProjectDTO.cs
+++ TheBugTracker.Client/Models/ProjectDTO.cs
@@ -1,5 +1,7 @@
 ﻿using TheBugTracker.Client.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace TheBugTracker.Client.Models
 {
@@ -10,15 +12,19 @@ namespace TheBugTracker.Client.Models
         private DateTimeOffset? _startDate;
         private DateTimeOffset? _endDate;
 
+        [Description("The unique identifier for the project")]
         public int Id { get; set; }
 
         [Required]
+        [Description("The name or title of the project")]
         public string? Name { get; set; }
 
         [Required]
+        [Description("A brief summary or details about the project")]
         public string? Description { get; set; }
 
         // From auto property to full property
+        [Description("The date and time when the project was created, stored in UTC")]
         public DateTimeOffset Created
         {
             get => _created;
@@ -26,6 +32,7 @@ namespace TheBugTracker.Client.Models
         }
 
         [Required]
+        [Description("The date and time when the project is scheduled to start, stored in UTC")]
         public DateTimeOffset? StartDate
         {
             get => _startDate;
@@ -33,27 +40,35 @@ namespace TheBugTracker.Client.Models
         }
 
         [Required]
+        [Description("The date and time when the project is scheduled to end, stored in UTC")]
         public DateTimeOffset? EndDate
         {
             get => _endDate;
             set => _endDate = value?.ToUniversalTime();
         }
 
+        [Description("The relative priority assigned to a project")]
         public ProjectPriority Priority { get; set; }
+
+        [Description("Indicates whether the project is active or archived")]
         public bool IsArchived { get; set; } = false;
+
+        [Description("The collection of tickets associated with the project")]
         public ICollection<TicketDTO> Tickets { get; set; } = [];
+
+        [Description("The collection of users associated with the project")]
         public ICollection<UserDTO> Members { get; set; } = [];
 
         #region Helper properties
 
-        [Required]
+        [Required, JsonIgnore]
         public DateTime? StartDateTime 
         { 
             get => StartDate?.DateTime;
             set => StartDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : null;
         }
 
-        [Required]
+        [Required, JsonIgnore]
         public DateTime? EndDateTime
         {
             get => EndDate?.DateTime;
