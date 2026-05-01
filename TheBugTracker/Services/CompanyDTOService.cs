592bbcb0bf67ec7df5619584865f520ce72f7aac
justarunsthing <justarunsthing@outlook.com>
2026-05-01 08:21:53 +0100

    Added CompanyDTOService with GetUsersAsync method implementation

---
+++ TheBugTracker/Services/CompanyDTOService.cs
@@ -0,0 +1,25 @@
+﻿using TheBugTracker.Client;
+using TheBugTracker.Models;
+using TheBugTracker.Interfaces;
+using TheBugTracker.Client.Enums;
+using TheBugTracker.Client.Models;
+using TheBugTracker.Client.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class CompanyDTOService(ICompanyRepository repository) : ICompanyDTOService
+    {
+        public async Task<IEnumerable<UserDTO>> GetUsersAsync(UserInfo userInfo)
+        {
+            IEnumerable<ApplicationUser> users = await repository.GetUsersAsync(userInfo);
+            var dtos = users.Select(u => u.ToDTO());
+
+            return dtos;
+        }
+
+        public Task<IEnumerable<UserDTO>> GetUsersInRoleAsync(Role role, UserInfo userInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
