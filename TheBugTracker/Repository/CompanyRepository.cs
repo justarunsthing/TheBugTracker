e7524a5f4540dbbd7f9bf92ef46de61049e467f2
justarunsthing <justarunsthing@outlook.com>
2026-05-01 08:14:45 +0100

    Added skeleton CompanyRepository

---
+++ TheBugTracker/Repository/CompanyRepository.cs
@@ -0,0 +1,22 @@
+﻿using TheBugTracker.Data;
+using TheBugTracker.Client;
+using TheBugTracker.Models;
+using TheBugTracker.Interfaces;
+using TheBugTracker.Client.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBugTracker.Repository
+{
+    public class CompanyRepository(IDbContextFactory<ApplicationDbContext> contextFactory) : ICompanyRepository
+    {
+        public Task<IEnumerable<ApplicationUser>> GetUsersAsync(UserInfo userInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ApplicationUser>> GetUsersInRoleAsync(Role role, UserInfo userInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
