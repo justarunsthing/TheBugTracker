92da22554c6ec66240c3a802a8a3c137147d0043
justarunsthing <justarunsthing@outlook.com>
2026-05-01 08:13:11 +0100

    Added ICompanyRepository interface

---
+++ TheBugTracker/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,22 @@
+﻿using TheBugTracker.Client;
+using TheBugTracker.Models;
+using TheBugTracker.Client.Enums;
+
+namespace TheBugTracker.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        /// <summary>
+        /// Get all users in the current user's company
+        /// </summary>
+        /// <param name="userInfo">The current user's claims</param>
+        Task<IEnumerable<ApplicationUser>> GetUsersAsync(UserInfo userInfo);
+
+        /// <summary>
+        /// Get all users in the current user's company in a specific role
+        /// </summary>
+        /// <param name="role">The role assigned to the users</param>
+        /// <param name="userInfo">The current user's claims</param>
+        Task<IEnumerable<ApplicationUser>> GetUsersInRoleAsync(Role role, UserInfo userInfo);
+    }
+}
