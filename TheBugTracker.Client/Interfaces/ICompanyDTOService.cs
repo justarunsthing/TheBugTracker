f99e2a2d1c4ab6c7808c1b60be0c87337202b610
justarunsthing <justarunsthing@outlook.com>
2026-05-01 08:18:44 +0100

    Added ICompanyDTOService

---
+++ TheBugTracker.Client/Interfaces/ICompanyDTOService.cs
@@ -0,0 +1,21 @@
+﻿using TheBugTracker.Client.Enums;
+using TheBugTracker.Client.Models;
+
+namespace TheBugTracker.Client.Interfaces
+{
+    public interface ICompanyDTOService
+    {
+        /// <summary>
+        /// Get all users in the current user's company
+        /// </summary>
+        /// <param name="userInfo">The current user's claims</param>
+        Task<IEnumerable<UserDTO>> GetUsersAsync(UserInfo userInfo);
+
+        /// <summary>
+        /// Get all users in the current user's company in a specific role
+        /// </summary>
+        /// <param name="role">The role assigned to the users</param>
+        /// <param name="userInfo">The current user's claims</param>
+        Task<IEnumerable<UserDTO>> GetUsersInRoleAsync(Role role, UserInfo userInfo);
+    }
+}
