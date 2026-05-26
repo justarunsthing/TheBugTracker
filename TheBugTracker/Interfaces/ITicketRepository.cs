60c7e1fade19eb4859ec078258ac8f894ae6e770
justarunsthing <justarunsthing@outlook.com>
2026-05-26 08:15:02 +0100

    Added skeleton ticket repository service & interface

---
+++ TheBugTracker/Interfaces/ITicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using TheBugTracker.Client;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Interfaces
+{
+    public interface ITicketRepository
+    {
+        /// <summary>
+        /// Retrives all open tickets in the user's company
+        /// </summary>
+        /// <param name="user">The current user's claims</param>
+        Task<IEnumerable<Ticket>> GetOpenTicketsAsync(UserInfo user);
+    }
+}
---
+++ TheBugTracker/Repository/TicketRepository.cs
@@ -0,0 +1,17 @@
+﻿using TheBugTracker.Data;
+using TheBugTracker.Client;
+using TheBugTracker.Models;
+using TheBugTracker.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBugTracker.Repository
+{
+    public class TicketRepository(IDbContextFactory<ApplicationDbContext> contextFactory, UserManager<ApplicationUser> userManager) : ITicketRepository
+    {
+        public Task<IEnumerable<Ticket>> GetOpenTicketsAsync(UserInfo user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
