627d6a9affbdc96647083d9988da4072c473f3fd
justarunsthing <justarunsthing@outlook.com>
2026-06-24 17:03:24 +1000

    Update ticket

--- TheBugTracker/Services/TicketDTOService.cs
+++ TheBugTracker/Services/TicketDTOService.cs
@@ -137,6 +137,8 @@ namespace TheBugTracker.Services
                     }
                 }
             }
+
+            await repository.UpdateTicketAsync(dbTicket, userInfo);
         }
     }
 }
