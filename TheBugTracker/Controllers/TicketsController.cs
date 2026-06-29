d06b4e7e47721514d17b2a63313f55046d417381
justarunsthing <justarunsthing@outlook.com>
2026-06-29 07:48:29 +0100

    Added skeleton TicketsController

---
+++ TheBugTracker/Controllers/TicketsController.cs
@@ -0,0 +1,16 @@
+﻿using TheBugTracker.Client;
+using Microsoft.AspNetCore.Mvc;
+using TheBugTracker.Client.Helpers;
+using TheBugTracker.Client.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TheBugTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TicketsController(ITicketDTOService ticketService) : ControllerBase
+    {
+        private UserInfo UserInfo => UserInfoHelper.GetUserInfo(User)!;
+    }
+}
