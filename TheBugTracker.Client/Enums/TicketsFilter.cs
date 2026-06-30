203952a741d5331710d53341fc1f5d75792e19c0
justarunsthing <justarunsthing@outlook.com>
2026-06-30 07:57:49 +0100

    Added TicketsFilter enum

---
+++ TheBugTracker.Client/Enums/TicketsFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheBugTracker.Client.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TicketsFilter
+    {
+        Open,
+        Resolved,
+        Assigned,
+        Archived
+    }
+}
