ec48eada9863cd8b9a5d2c92e1f17b29a5ee03cf
justarunsthing <justarunsthing@outlook.com>
2026-07-27 08:06:14 +0100

    Added BrowserFileHelper

---
+++ TheBugTracker.Client/Helpers/BrowserFileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace TheBugTracker.Client.Helpers
+{
+    public static class BrowserFileHelper
+    {
+        public static readonly int MaxFileSize = 5 * 1024 * 1024; // 5 MB
+
+        public static async Task<byte[]> GetFileDataAsync(IBrowserFile file)
+        {
+            if (file.Size < MaxFileSize)
+            {
+                try
+                {
+                    await using Stream fileStream = file.OpenReadStream(MaxFileSize);
+                    await using MemoryStream ms = new();
+
+                    await fileStream.CopyToAsync(ms);
+
+                    return ms.ToArray();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            throw new IOException("File was either invalid or too large!");
+        }
+    }
+}
