8543d9dc0e3267d70f9e4f1ae64a76c2668b9390
justarunsthing <justarunsthing@outlook.com>
2026-08-01 07:53:10 +0100

    Added endpoint to download ticket attachment

--- TheBugTracker/Controllers/UploadsController.cs
+++ TheBugTracker/Controllers/UploadsController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
+using TheBugTracker.Client;
+using TheBugTracker.Client.Helpers;
 using TheBugTracker.Data;
+using TheBugTracker.Models;
 
 namespace TheBugTracker.Controllers
 {
@@ -23,6 +27,27 @@ namespace TheBugTracker.Controllers
             }
 
             return File(image.Data!, image.Type!);
+        }
+
+        // New endpoint for /api/attachments
+        [HttpGet("/api/attachments/{uploadId:guid}")]
+        [Authorize]
+        public async Task<ActionResult> DownloadAttachmentAsync([FromRoute] Guid uploadId)
+        {
+            UserInfo userInfo = UserInfoHelper.GetUserInfo(User)!;
+            TicketAttachment? attachment = await context.Attachments
+                .Include(a => a.Upload)
+                .FirstOrDefaultAsync(a => a.UploadId == uploadId && a.Ticket!.Project!.CompanyId == userInfo.CompanyId);
+
+            if (attachment is null)
+            {
+                return NotFound();
+            }
+
+            // Set the Content-Disposition header to indicate that the file should be downloaded as an attachment
+            Response.Headers.TryAdd("Content-Disposition", $"attachment; filename={attachment.FileName}");
+
+            return File(attachment.Upload!.Data!, attachment.Upload!.Type!, attachment.FileName);
         }
     }
 }
