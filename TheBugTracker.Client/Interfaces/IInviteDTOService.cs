702c8e08f5844b33256069fd4b6398043e56489c
justarunsthing <justarunsthing@outlook.com>
2026-09-10 05:44:55 +0100

    Added invite interface

---
+++ TheBugTracker.Client/Interfaces/IInviteDTOService.cs
@@ -0,0 +1,15 @@
+﻿using TheBugTracker.Client.Models;
+
+namespace TheBugTracker.Client.Interfaces
+{
+    public interface IInviteDTOService
+    {
+        /// <summary>
+        /// Saves a new invite for the user's company to the database. Only admins may create invites.
+        /// </summary>
+        /// <param name="invite">The details of the invite to create</param>
+        /// <param name="userInfo">The current user's claims</param>
+        /// <returns>The created invite</returns>
+        Task<InviteDTO> CreateInviteAsync(InviteDTO invite, UserInfo userInfo);
+    }
+}
---
+++ TheBugTracker/Interfaces/IInviteRepository.cs
@@ -0,0 +1,16 @@
+﻿using TheBugTracker.Client;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Interfaces
+{
+    public interface IInviteRepository
+    {
+        /// <summary>
+        /// Saves a new invite for the user's company to the database. Only admins may create invites.
+        /// </summary>
+        /// <param name="invite">The details of the invite to create</param>
+        /// <param name="userInfo">The current user's claims</param>
+        /// <returns>The created invite</returns>
+        Task<Invite> CreateInviteAsync(Invite invite, UserInfo userInfo);
+    }
+}
