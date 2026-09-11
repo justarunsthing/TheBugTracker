61286db1c823f8e7ec7286fb5409d2c6f5329e6a
justarunsthing <justarunsthing@outlook.com>
2026-09-11 08:31:22 +0100

    Implemented CreateInviteAsync in invite repository

---
+++ TheBugTracker/Repository/InviteRepository.cs
@@ -0,0 +1,45 @@
+﻿using TheBugTracker.Data;
+using TheBugTracker.Client;
+using TheBugTracker.Models;
+using TheBugTracker.Interfaces;
+using TheBugTracker.Client.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBugTracker.Repository
+{
+    public class InviteRepository(IDbContextFactory<ApplicationDbContext> contextFactory) : IInviteRepository
+    {
+        public async Task<Invite> CreateInviteAsync(Invite invite, UserInfo userInfo)
+        {
+            if (!userInfo.IsInRole(Role.Admin))
+            {
+                throw new ApplicationException($"{userInfo.Email} is not authorized to create invites.");
+            }
+
+            await using ApplicationDbContext context = contextFactory.CreateDbContext();
+
+            if (await context.Users.AnyAsync(u => u.Email!.ToUpper() == invite.InviteeEmail!.ToUpper()))
+            {
+                throw new ApplicationException($"The email {invite.InviteeEmail} is already registered.");
+            }
+
+            if (!await context.Projects.AnyAsync(p => p.Id == invite.ProjectId && p.CompanyId == userInfo.CompanyId))
+            {
+                throw new ApplicationException($"Project ID: {invite.ProjectId} does not exist in the company.");
+            }
+
+            invite.CompanyToken = Guid.NewGuid();
+            invite.CompanyId = userInfo.CompanyId;
+            invite.InvitorId = userInfo.UserId;
+            invite.InviteDate = DateTimeOffset.UtcNow;
+            invite.IsValid = true;
+            invite.InviteeId = null;
+            invite.JoinDate = null;
+
+            context.Invites.Add(invite);
+            await context.SaveChangesAsync();
+
+            return invite;
+        }
+    }
+}
