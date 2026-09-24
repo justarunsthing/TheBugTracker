94d0f62a8a905f02272f27c6e2fe8906b0aa9cb3
justarunsthing <justarunsthing@outlook.com>
2026-09-24 06:08:39 +0100

    Implemented SendInviteAsync method

--- TheBugTracker/Repository/InviteRepository.cs
+++ TheBugTracker/Repository/InviteRepository.cs
@@ -107,7 +107,85 @@ namespace TheBugTracker.Repository
 
         public async Task<bool> SendInviteAsync(Uri baseUri, int inviteId, UserInfo userInfo)
         {
+            if (!userInfo.IsInRole(Role.Admin))
+            {
+                return false;
+            }
 
+            try
+            {
+                await using ApplicationDbContext context = _contextFactory.CreateDbContext();
+
+                Invite? invite = await context.Invites
+                    .Include(i => i.Company)
+                    .Include(i => i.Invitor)
+                    .Include(i => i.Project)
+                    .FirstOrDefaultAsync(i => i.Id == inviteId
+                                              && i.CompanyId == userInfo.CompanyId
+                                              && i.IsValid == true);
+
+                if (invite is null || ValidateInvite(invite) == false)
+                {
+                    if (invite is not null)
+                    {
+                        invite.IsValid = false;
+                        await context.SaveChangesAsync();
+                    }
+
+                    return false;
+                }
+
+                string protectedEmail = _protector.Protect(invite.InviteeEmail!);
+                string protectedCompanyId = _protector.Protect(invite.CompanyId.ToString());
+                string protectedToken = _protector.Protect(invite.CompanyToken.ToString());
+
+                // https://mybugtracker.com OR https://localhost:5001
+                string baseUrl = baseUri.GetLeftPart(UriPartial.Authority);
+                string inviteUrl = $"{baseUrl}/Account/Register/Invite?token={protectedToken}&email={protectedEmail}&companyId={protectedCompanyId}";
+
+                string subject = $"You have been invited to join {invite.Company!.Name} on The Bug Tracker!";
+                string message = string.IsNullOrEmpty(invite.Message)
+                    ? string.Empty
+                    : $"""
+                        <hr />
+                        <p>Message from {invite.Invitor!.FirstName} {invite.Invitor.LastName}:</p>
+                        <blockquote>{invite.Message}</blockquote>
+                        <hr />
+                       """;
+
+                string body = $"""
+                                <h1>Welcome to The Bug Tracker!</h1>
+                                <p>
+                                    {invite.Invitor!.FirstName} {invite.Invitor.LastName} has invited you to 
+                                    join {invite.Company!.Name} on The Bug Tracker to work on the project
+                                    {invite.Project!.Name}!
+                                </p>
+                                {message}
+                                <p>
+                                    To accept this invitation, pleaes <a href="{inviteUrl}">click here</a>
+                                    to register a new account.
+                                </p>
+                                <p>
+                                    <strong>Note:</strong> This invitation will expire on {invite.InviteDate.AddDays(7):d}
+                                </p>
+                                <small>
+                                    If you are unable to click the link above, please copy & paste the following URL into
+                                    your browser's address bar:
+                                    <br />
+                                    {inviteUrl}
+                                </small>
+                               """;
+
+                await _emailSender.SendEmailAsync(invite.InviteeEmail!, subject, body);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
         }
 
         private bool ValidateInvite(Invite invite)
